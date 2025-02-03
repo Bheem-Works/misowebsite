7d5cf1f7e43132b0b419ba01b2a6450bce86b056
Bhim Magar <shokokomichan098@gmail.com>
2025-02-04 02:17:57 +0545

    responsive changes

---
+++ script/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menu = document.querySelector(".nav-links");
+    const hamburger = document.querySelector(".hamburger");
+
+    hamburger.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
+});
